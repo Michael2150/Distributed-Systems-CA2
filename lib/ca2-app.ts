366c2b1dc02e2217a8066c16f7aa471852f3463e
Michael Gerber <michaelgerb2150@gmail.com>
2024-01-02 23:01:32 +0000

    broke

--- lib/ca2-app.ts
+++ lib/ca2-app.ts
@@ -64,6 +64,38 @@ export class CA2AppStack extends cdk.Stack {
     // Permissions
     imagesBucket.grantRead(processImageFn);
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    }); // 5 messages at a time 
+
+    mailerFn.addEventSource(newImageMailEventSource);
+
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+
     // Output
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
